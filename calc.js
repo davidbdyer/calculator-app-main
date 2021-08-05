9f1ea840ebc91e64148bc6cb36587d6b3f485623
davidbdeath <social_davidbdyer@icloud.com>
2021-08-05 01:19:18 -0400

    keyboard enabled

--- calc.js
+++ calc.js
@@ -1,4 +1,30 @@
 const del = function () {
-    const value = document.getElementById('screen').value;
-    document.getElementById('screen').value = value.substr(0, value.length - 1);
+    const value = document.getElementById('answer').value;
+    document.getElementById('answer').value = value.substr(0, value.length - 1);
 }
+
+addEventListener("keypress", function(event){
+    const { key } = event
+    if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '.'].includes(key) === true) {
+        calcDisplay.answer.value+=key
+    }
+})
+
+addEventListener("keypress", function(event){
+    const { key } = event
+    if (key === "Enter" && calcDisplay.answer.value !== "") {
+        calcDisplay.answer.value = eval(calcDisplay.answer.value)
+    }
+})
+
+addEventListener("keypress", function(event){
+    const { key } = event
+    if( key === 'Backspace') {
+        del();
+    }
+})
+
+addEventListener("keypress", function(event){
+    const { key } = event
+    console.log(key);
+})
