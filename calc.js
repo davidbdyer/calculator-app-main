61b52fd5533accd925138f070f3d4eba94903e15
davidbdeath <social_davidbdyer@icloud.com>
2021-08-05 15:53:16 -0400

    changed listner from keypress to keydown

--- calc.js
+++ calc.js
@@ -3,28 +3,29 @@ const del = function () {
     document.getElementById('answer').value = value.substr(0, value.length - 1);
 }
 
-addEventListener("keypress", function(event){
+addEventListener("keydown", function(event){
     const { key } = event
     if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '.'].includes(key) === true) {
         calcDisplay.answer.value+=key
     }
 })
 
-addEventListener("keypress", function(event){
+addEventListener("keydown", function(event){
     const { key } = event
     if (key === "Enter" && calcDisplay.answer.value !== "") {
         calcDisplay.answer.value = eval(calcDisplay.answer.value)
     }
 })
 
-addEventListener("keypress", function(event){
+addEventListener("keydown", function(event){
     const { key } = event
-    if( key === 'Backspace') {
+    if( key === 'Backspace' || key === 'Delete' || key === 'Clear') {
         del();
     }
 })
 
-addEventListener("keypress", function(event){
+addEventListener("keydown", function(event){
     const { key } = event
     console.log(key);
 })
+
