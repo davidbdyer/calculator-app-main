6d51bb540dc7ce3802a485f00c0ac044158a5b4a
davidbdeath <social_davidbdyer@icloud.com>
2021-08-06 14:37:15 -0400

    input verification fully implemented

--- calc.js
+++ calc.js
@@ -1,83 +1,77 @@
-const operators = ['+', '-', '*', '/', '.'];
+const operators = ['+', '-', '*', '/'];
 const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-const buttons = [...document.querySelectorAll('button')];
+const equalInputs = ['Enter', '='];
+const deleteInputs = ['Backspace', 'Delete', 'Clear', 'DEL'];
 let inputBuffer;
-
-const del = function () {
-    const value = document.getElementById('screen').value;
-    document.getElementById('screen').value = value.substr(0, value.length - 1);
-}
 
 const printToScreen = function () {
     calcDisplay.screen.value+=inputBuffer;
     console.log(inputBuffer);
 }
 
-const mouseOperator = function () {
+const del = function () {
+    if (deleteInputs.includes(inputBuffer) === true && calcDisplay.screen.value !== '') {
+        const value = document.getElementById('screen').value;
+        document.getElementById('screen').value = value.substr(0, value.length - 1);
+    }
+}
+
+const operatorInput = function () {
     if ( operators.includes(inputBuffer) === true && operators.includes(calcDisplay.screen.value.slice(-1)) === false ) {
         printToScreen();
     };
 
 }
 
-const mouseNumbers = function () {
+const numberInput = function () {
     if ( numbers.includes(inputBuffer) === true )
     printToScreen();
 }
 
-// mouse input
+const equalInput = function () {
+    if ( equalInputs.includes(inputBuffer) === true && calcDisplay.screen.value !== '') {
+        calcDisplay.screen.value = eval(calcDisplay.screen.value)
+    }
+}
+
+const decimalInput = function () {
+    const splitArray = calcDisplay.screen.value.split(/[\*\+\-\/]/);
+    const lastArrayElement = splitArray[splitArray.length - 1];
+    if ( ['.'].includes(inputBuffer) && !( lastArrayElement.includes('.') ) ) {
+        printToScreen();
+        console.log(`last element fail ${lastArrayElement}`)
+    } else {
+        console.log(`last element ${lastArrayElement}`);
+    }
+}
+
+// Mouse Input
+const buttons = [...document.querySelectorAll('button')];
 buttons.forEach((btn) => {
     btn.addEventListener('click', function() {
         inputBuffer = btn.textContent;
-        mouseOperator();
-        mouseNumbers();
+        operatorInput();
+        numberInput();
+        equalInput();
+        del();
+        decimalInput();
     })
-})
+});
 
 // Keyboard Input
-// number keys
 addEventListener('keydown', function(event){
     const { key } = event
-    if (numbers.includes(key) === true) {
-        calcDisplay.screen.value+=key
-    }
-})
-
-// operator keys
-addEventListener('keydown', function(event){
-    const { key } = event
-    if ( operators.includes(key) === true && operators.includes(calcDisplay.screen.value.slice(-1)) === false ) {
-    	calcDisplay.screen.value+=key
-    }
-})
-
-// // limit decimel
-// addEventListener('keydown', function(event){
-//     const { key } = event
-//     if ( key === '.' && calcDisplay.screen.value.includes('.') === false ) {
-//     	calcDisplay.screen.value+=key
-//     }
-// })
-
-// Enter to equals
-addEventListener('keydown', function(event){
-    const { key } = event
-    if (key === 'Enter' && calcDisplay.screen.value !== '') {
-        calcDisplay.screen.value = eval(calcDisplay.screen.value)
-    }
-})
-
-// delete keys
-addEventListener('keydown', function(event){
-    const { key } = event
-    if( key === 'Backspace' || key === 'Delete' || key === 'Clear') {
-        del();
-    }
-})
+    inputBuffer = key;
+    operatorInput();
+    numberInput();
+    equalInput();
+    del();
+    decimalInput();
+});
 
 // keys to console
-addEventListener('keydown', function(event){
-    const { key } = event
-    // console.log(key);
-    console.log(key)
-})
+// addEventListener('keydown', function(event){
+//     const { key } = event
+//     // console.log(key);
+//     console.log(key)
+// })
