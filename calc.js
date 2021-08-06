9a743cbc6dd8badd956ac4f523748b3fd21521be
davidbdeath <social_davidbdyer@icloud.com>
2021-08-05 21:08:24 -0400

    key validation added

--- calc.js
+++ calc.js
@@ -1,15 +1,30 @@
+const operatorKeys = ['+', '-', '*', '/', '.'];
+const numberKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+calcDisplay.answer.value.slice(-2);
+
+
 const del = function () {
     const value = document.getElementById('answer').value;
     document.getElementById('answer').value = value.substr(0, value.length - 1);
 }
 
+// number keys
 addEventListener("keydown", function(event){
     const { key } = event
-    if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '.'].includes(key) === true) {
+    if (numberKeys.includes(key) === true) {
         calcDisplay.answer.value+=key
     }
 })
 
+// operator keys
+addEventListener("keydown", function(event){
+    const { key } = event
+    if ( operatorKeys.includes(key) === true && operatorKeys.includes(calcDisplay.answer.value.slice(-1)) === false ) {
+    	calcDisplay.answer.value+=key
+    }
+})
+
+// Enter to equals
 addEventListener("keydown", function(event){
     const { key } = event
     if (key === "Enter" && calcDisplay.answer.value !== "") {
@@ -17,6 +32,7 @@ addEventListener("keydown", function(event){
     }
 })
 
+// delete function
 addEventListener("keydown", function(event){
     const { key } = event
     if( key === 'Backspace' || key === 'Delete' || key === 'Clear') {
@@ -24,8 +40,10 @@ addEventListener("keydown", function(event){
     }
 })
 
+// keys to console
 addEventListener("keydown", function(event){
     const { key } = event
-    console.log(key);
+    // console.log(key);
+    console.log(key)
 })
 
