185879de5f94a2e5a206bc04c38443173310cefa
davidbdeath <social_davidbdyer@icloud.com>
2021-12-07 01:18:41 +0000

    aria labels added

--- theme-switcher.js
+++ theme-switcher.js
@@ -1,19 +1,19 @@
 function setStyleSheet(url) {
-    let stylesheet = document.getElementById("theme");
-    stylesheet.setAttribute('href', url);
+	let stylesheet = document.getElementById('theme');
+	stylesheet.setAttribute('href', url);
 }
 
 const checkPrefersLightMode = function () {
-    const { matches } = window.matchMedia('(prefers-color-scheme: light)')
-    if (matches === true) {
-        setStyleSheet('theme2.css')
-        document.getElementById('theme-2').checked = true;
-    }
-}
+	const { matches } = window.matchMedia('(prefers-color-scheme: light)');
+	if (matches === true) {
+		setStyleSheet('theme2.css');
+		document.getElementById('theme-2').checked = true;
+	}
+};
 
 onload = checkPrefersLightMode();
 
-document.addEventListener("dblclick", event => {
-    event.preventDefault()
-    event.stopPropagation()
-  })
+document.addEventListener('dblclick', (event) => {
+	event.preventDefault();
+	event.stopPropagation();
+});
